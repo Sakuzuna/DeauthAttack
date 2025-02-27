f479c7f0feeb3cca2a73e4123d7cf936118a32b2
Sakuzuna <nikita.kopychev@s.wingateschool.com>
2025-02-27 03:46:30 +0000

    Create install.py

---
+++ install.py
@@ -0,0 +1,6 @@
+import subporcess
+
+subprocess.run(['npm', 'init', '-y'])
+subprocess.run(['npm', 'install', 'express', 'ts-node', 'typescript', '@types/node', '@types/express'])
+subprocess.run(['npm', 'install'])
+subprocess.run(['npm', 'run', 'dev'])
