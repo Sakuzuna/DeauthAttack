23bb6ffcf45b506927dd0b57ffe8acb2e8d89bd2
Sakuzuna <nikita.kopychev@s.wingateschool.com>
2025-02-27 04:08:49 +0000

    Delete install.py

--- install.py
+++
@@ -1,6 +0,0 @@
-import subprocess
-
-subprocess.run(['npm', 'init', '-y'])
-subprocess.run(['npm', 'install', 'express', 'ts-node', 'typescript', '@types/node', '@types/express'])
-subprocess.run(['npm', 'install'])
-subprocess.run(['npm', 'run', 'dev'])
